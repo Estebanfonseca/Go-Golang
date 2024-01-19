8a6fb9f65042b08809e594106eb1ede85a67ab6f
ext-efonseca_Agro <estebanp57@hotmail.com>
2024-01-19 10:52:49 -0500

    ciclos y funciones

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// ? se usa coma y luego el tipo para saber que esos argumentos son de ese tipo
+func dobleNumero(a,b int){
+	fmt.Printf("%d y %d es igual a : %d\n",a,b,a + b)
+}
+
+// ? para que una funcion retorne 2 valores se especifica con otro ()
+
+func doble(a int) (b,c int) {
+	return a , a *4
+}
+
 func main() {
 	fmt.Println("hola mundo")
 
@@ -67,5 +78,47 @@ func main() {
 	// * Sprintf no imprime en consola solo guarda como string
 	message := fmt.Sprintf("%s tiene %d",nombre,age)
 	fmt.Println(message)
+
+	dobleNumero(5,8)
+	dobleNumero(14,22)
+
+	//? para las funciones con retorno de varios datos se guardan en numero de variables que retorna si en caso que no quiera algun dato lo especificamos con _ 
+
+
+	value1 , value2 := doble(8)
+
+	fmt.Println(value1,value2)
+
+	value3 , _ := doble(13)
+
+	fmt.Println("se quiere el primer valor",value3)
+
+	_,value4 := doble(25)
+
+	fmt.Println("se quiere el segundo valor",value4)
+
+	// ? ciclo for igual que en javascript
+
+	for i := 0; i < 10 ; i++{
+		fmt.Println("ciclo",i)
+	}
+
+	// ? for while 
+
+	counter := 0
+
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	//? for forever este ciclo es infinito 
+	// forever := 0
+	// for{
+	// 	fmt.Println(forever)
+	// 	forever++
+	// }
+
+	
 
 }
